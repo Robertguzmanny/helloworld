e839917b0fa3d48f51d5fa7932fa7c1d991065b3
Robertguzmanny <101305148+Robertguzmanny@users.noreply.github.com>
2022-08-30 00:23:12 -0400

    Application 1 with a happy birthday

--- app/src/main/java/com/example/myapplication1/MainActivity.java
+++ app/src/main/java/com/example/myapplication1/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.myapplication1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final String s = "Here is one tag";
+        
+
+
     }
 }
